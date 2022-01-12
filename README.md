f9be79ff7d3b460a383bc606112873362ef17dce
tmantena1 <tmantena1@gmail.com>
2022-01-12 16:17:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# E-commerce Back End
+
+## Description 
+The goal of this assignment is to build the back end for an e-commerce site by modifying starter code. This will be done by configuring a working Express.js API to use Sequelize to interact with a MySQL database.
+
+## Table of Contents
+* [Installation](#installation)
+* [Walkthrough](#walkthrough)
+
+## Installation
+```
+1. Download and install Insomnia
+2. Run 'npm install'
+3. Run 'npm seed' to seed the database
+4. Run 'npm start'
+5. Use the routes on Insomnia
+```
+
+## Walkthrough
---
+++ routes/api/category-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  const categoryData = await Category.findAll( {
+    include: [{model: Product}]
+  })
+  return res.json(categoryData);
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  const categoryData = await Category.create(req.body);
+  return res.json(categoryData);
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  const categoryData = await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+
+  return res.json(categoryData);
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
+  // return res.json(categoryData);
+});
+
+module.exports = router;
